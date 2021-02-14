9438caeeb148b3f11271c41fc6d6c1480b067492
Nadeeya Bunya <5910110167@psu.ac.th>
2021-02-14 15:35:41 +0700

    commit

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post,Photo
+
+def post_list(request):
+    posts = Post.objects.order_by('-created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def photo_list(request):
+    # posts = Post.objects.order_by('-created_date')
+    return render(request, 'blog/photo_list.html', {})
